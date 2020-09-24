954ad60b94580467390cbe51fdbc9ce34166590a
Jyke Savia <jyke.savia@organization>
2020-09-24 08:50:51 +0300

    initial examples

---
+++ simple-expression-example/lexer.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import sys, ply.lex
+
+tokens = (
+    'NUMBER',
+    'PLUS','MINUS','TIMES','DIVIDE',
+    'LPAREN','RPAREN',
+    )
+
+# Tokens
+
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+
+def t_NUMBER(t):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print("Integer value too large %d", t.value)
+        t.value = 0
+    return t
+
+# Ignored characters
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+def t_error(t):
+    raise Exception("Illegal character '{}' at line {}".format( 
+        t.value[0], t.lexer.lineno ) )
+
+# define lexer in module level so it can be used after 
+# importing this module:
+lexer = ply.lex.lex()
+
+# if this module/file is the first one started (the main module)
+# then run:
+if __name__ == '__main__':
+    import argparse, codecs
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--who', action='store_true', help='who wrote this' )
+    group.add_argument('-f', '--file', help='filename to process')
+
+    ns = parser.parse_args()
+    if ns.who == True:
+        # identify who wrote this
+        print( '88888 Ahto Simakuutio' )
+    elif ns.file is None:
+        # user didn't provide input filename
+        parser.print_help()
+    else:
+        # using codecs to make sure we process unicode
+        with codecs.open( ns.file, 'r', encoding='utf-8' ) as INFILE:
+            # blindly read all to memory (what if that is a 42Gb file?)
+            data = INFILE.read() 
+
+        lexer.input( data )
+
+        while True:
+            token = lexer.token()
+            if token is None:
+                break
+            print( token )
+
